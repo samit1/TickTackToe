a384a2bd1e0e4b460b65251880b09d156a1a4310
Sami Taha <taha@uber.com>
2018-07-31 20:17:03 +0300

    Added function to return views in array format so controller can handle adding actions to it

--- TickTackToe/Views/TickTackToeViewContainer.swift
+++ TickTackToe/Views/TickTackToeViewContainer.swift
@@ -10,7 +10,10 @@ import Foundation
 import UIKit
 class SquareGridContainer : UIView {
     
-    private var stackViews = [HorizontalStackView]()
+    /// Flag on whether to add view constraints
+    private var viewsNeedConstains = true
+    
+    /// Vertical UIStackView which contains the Grid
     private var containingStackView : UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -18,16 +21,31 @@ class SquareGridContainer : UIView {
         return stackView
     }()
     
-    func configureGridWithViews(numberOfTotalItems : Int, numberPerRow: Int, viewType: UIView.Type) {
-        stackViews.removeAll()
+    /// - returns: The UIViews which are contained within in the Grid
+    func getViewsInGrid() -> [UIView] {
+        var views = [UIView]()
         
+        for view in containingStackView.arrangedSubviews {
+            if let stackView = view as? HorizontalStackView {
+                views += stackView.arrangedSubviews
+            }
+        }
+        
+        return views
+    }
+ 
+    /// - parameter numberOfTotalItems: The total number of UIView types that should be shown in the Grid
+    /// - parameter numberPerRow: The total number of items each that should be in each row of the swuare grid
+    /// - parameter viewType: The type of view that is being added to the grid
+    /// - note: A fatal errror will occur if you try to configuere a non-square grid. For example a 3x3 grid is a square, but a 4x3 grid is not. 
+    func configureGridWithViews(numberOfTotalItems : Int, numberPerRow: Int, viewType: UIView.Type) {
         /// Calculate number of rows required
         var rows = calculateRequiredRows(numberOfTotalItems: numberOfTotalItems, numberPerRow: numberPerRow)
         while rows >= 0 {
             
             var numLeft = numberPerRow
             let rowStackView = HorizontalStackView()
-            stackViews.append(rowStackView)
+            containingStackView.addArrangedSubview(rowStackView)
             while numLeft >= 0 {
                 
                 rowStackView.addArrangedSubview(viewType.init())
@@ -46,5 +64,16 @@ class SquareGridContainer : UIView {
         return numberOfTotalItems / numberPerRow
     }
     
-    
+    override func updateConstraints() {
+        if viewsNeedConstains {
+            viewsNeedConstains = !viewsNeedConstains
+            
+            let margins = safeAreaLayoutGuide
+            containingStackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+            containingStackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+            containingStackView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+            containingStackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        }
+        super.updateConstraints()
+    }
 }
